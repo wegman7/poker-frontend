c92519ca26ada39069f590e5cf8ccf008e3a8bfc
Josh Wegman <joshua.wegman@flooranddecor.com>
2025-01-17 20:05:33 -0500

    chips complete

--- src/app/room/Chip.tsx
+++ src/app/room/Chip.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Chip = ({ amount, color }: { amount: number, color: string }) => {
+const Chip = ({ amount, color, x, y }: { amount: number, color: string, x: string, y: string }) => {
+  const formatted = amount.toString().replace(/^0(?=\.)/, '');
   return (
-    <div className="chip-container">
+    <div className={`absolute chip-container`} style={{left: x, top: y}} >
       <div className="poker-chip" style={{ borderColor: color }}>
         <div className="chip-ring" style={{ borderColor: color }} />
-        <div className="chip-amount">{amount}</div>
+        <div className="chip-amount">{formatted}</div>
       </div>
     </div>
   );
--- src/app/room/Chips.tsx
+++ src/app/room/Chips.tsx
@@ -1,5 +1,12 @@
 import Chip from "./Chip";
 
+const x: { [key: string]: string } = {
+  brown: '0%',
+  gray: '25%',
+  red: '50%',
+  green: '75%',
+};
+
 const brownValue = .25;
 const grayValue = 1;
 const redValue = 5;
@@ -20,12 +27,23 @@ const calculateAmounts = (amount: number) => {
 const Chips = ({ size, position, amount }: { size: string, position: string, amount: number }) => {
   const { gray, brown, red, green } = calculateAmounts(amount);
 
+  const yDiff = -6;
   return (
-    <div className={`absolute transform -translate-x-1/2 -translate-y-1/2 ${size} ${position} flex bg-gray-300 rounded-lg text-gray-800`}>
-      <Chip amount={brown} color="brown" />
-      <Chip amount={gray} color="gray" />
-      <Chip amount={red} color="red" />
-      <Chip amount={green} color="green" />
+    <div className={`absolute transform -translate-x-1/2 -translate-y-1/2 ${size} ${position} bg-gray-300 rounded-lg text-gray-800`}>
+      <div className="relative w-full h-full">
+        {[...Array(gray)].map((_, index) => (
+          <Chip key={index} amount={brownValue} x={x['brown']} y={(index * yDiff).toString() + '%'} color="brown" />
+        ))}
+        {[...Array(brown)].map((_, index) => (
+          <Chip key={index} amount={grayValue} x={x['gray']} y={(index * yDiff).toString() + '%'} color="gray" />
+        ))}
+        {[...Array(red)].map((_, index) => (
+          <Chip key={index} amount={redValue} x={x['red']} y={(index * yDiff).toString() + '%'} color="red" />
+        ))}
+        {[...Array(green)].map((_, index) => (
+          <Chip key={index} amount={greenValue} x={x['green']} y={(index * yDiff).toString() + '%'} color="green" />
+        ))}
+      </div>
     </div>
   );
 };
