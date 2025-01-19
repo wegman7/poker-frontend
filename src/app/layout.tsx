e67f53d71455bc5f1557cec715e00c3536b78581
Josh Wegman <joshua.wegman@flooranddecor.com>
2025-01-19 17:09:57 -0500

    cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import "./globals.css";
-// import { ThemeProvider } from '@mui/material';
 
 export default function RootLayout({
   children,
