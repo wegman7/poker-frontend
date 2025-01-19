655b7cedb2a8b28bcb089f985d9567b065896a0b
Josh Wegman <joshua.wegman@flooranddecor.com>
2025-01-19 16:10:48 -0500

    save - BetButtons looks good

--- src/app/room/BetButtons.tsx
+++ src/app/room/BetButtons.tsx
@@ -22,19 +22,19 @@ const BetButtons: React.FC = () => {
 
   return (
     <div 
-      className="absolute top-[60%] left-1/2 transform -translate-x-1/2 bg-gray-800 text-white rounded-lg w-[50%] h-[30%]"
+      className="absolute w-[35%] left-[80%] top-[79%] transform -translate-x-1/2 text-white rounded-lg "
     >
-      <div className="flex justify-between w-full mb-4">
-        <button className="w-1/3 bg-gray-700 py-[5%] px-[2%] rounded-md hover:bg-gray-600 dynamic-text">FOLD</button>
-        <button className="w-1/3 bg-green-600 py-[5%] px-[2%] rounded-md hover:bg-green-500 dynamic-text">
-          CALL <br /> <span className="dynamic-text font-bold">200</span>
+      <div className="flex justify-between w-full mb-[2%]">
+        <button className="w-1/3 bg-gray-700 py-[1%] px-[2%] rounded-md hover:bg-gray-600 dynamic-text">FOLD</button>
+        <button className="w-1/3 bg-green-600 py-[1%] px-[2%] rounded-md hover:bg-green-500 dynamic-text">
+          CALL <br /> <span className="font-bold dynamic-text">200</span>
         </button>
-        <button className="w-1/3 bg-blue-600 py-[5%] px-[2%] rounded-md hover:bg-blue-500 dynamic-text">
-          RAISE TO <br /> <span className="dynamic-text font-bold">{betAmount}</span>
+        <button className="w-1/3 bg-blue-600 py-[1%] px-[2%] rounded-md hover:bg-blue-500 dynamic-text">
+          RAISE TO <br /> <span className="font-bold dynamic-text">{betAmount}</span>
         </button>
       </div>
 
-      <div className="flex items-center w-full mb-4">
+      <div className="flex items-center w-full mb-[2%]">
         <div className="flex items-center flex-grow">
           <button 
             className="bg-gray-600 py-[3%] px-[3%] rounded-l-md hover:bg-gray-500 dynamic-text"
@@ -48,7 +48,7 @@ const BetButtons: React.FC = () => {
             max="2000"
             value={betAmount}
             onChange={handleSliderChange}
-            className="w-full mx-2"
+            className="w-full mx-[2%]"
           />
           <button 
             className="bg-gray-600 py-[3%] px-[3%] rounded-r-md hover:bg-gray-500 dynamic-text"
@@ -61,31 +61,31 @@ const BetButtons: React.FC = () => {
           type="number"
           value={betAmount}
           onChange={handleInputChange}
-          className="ml-4 w-[15%] text-center bg-gray-900 rounded-md py-[3%] border border-gray-700 dynamic-text font-bold"
+          className="ml-[2%] w-[15%] text-center bg-gray-900 rounded-md py-[3%] border border-gray-700 font-bold dynamic-text"
         />
       </div>
 
       <div className="flex justify-between w-full">
         <button 
-          className="w-1/4 bg-gray-700 py-[5%] px-[2%] rounded-md hover:bg-gray-600 dynamic-text"
+          className="w-1/4 bg-gray-700 py-[2%] px-[2%] rounded-md hover:bg-gray-600 dynamic-text"
           onClick={() => handleButtonClick(3)}
         >
           X3
         </button>
         <button 
-          className="w-1/4 bg-gray-700 py-[5%] px-[2%] rounded-md hover:bg-gray-600 dynamic-text"
+          className="w-1/4 bg-gray-700 py-[2%] px-[2%] rounded-md hover:bg-gray-600 dynamic-text"
           onClick={() => handleButtonClick(5)}
         >
           X5
         </button>
         <button 
-          className="w-1/4 bg-gray-700 py-[5%] px-[2%] rounded-md hover:bg-gray-600 dynamic-text"
+          className="w-1/4 bg-gray-700 py-[2%] px-[2%] rounded-md hover:bg-gray-600 dynamic-text"
           onClick={() => setBetAmount(2000)}
         >
           Pot
         </button>
         <button 
-          className="w-1/4 bg-red-600 py-[5%] px-[2%] rounded-md hover:bg-red-500 dynamic-text"
+          className="w-1/4 bg-red-600 py-[2%] px-[2%] rounded-md hover:bg-red-500 dynamic-text"
           onClick={handleAllIn}
         >
           ALL-IN
