e3cc437af6d3f629413113f0c386ba63916cd8c8
Josh Wegman <joshua.wegman@flooranddecor.com>
2025-02-28 14:16:40 -0500

    cleanup

--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -2,7 +2,7 @@ import { Auth0Client } from "@auth0/nextjs-auth0/server"
 
 export const auth0 = new Auth0Client({
   authorizationParameters: {
-    audience: "http://localhost:8000",
+    audience: process.env.NEXT_PUBLIC_AUTH0_API_IDENTIFIER,
     clientAssertionSigningAlg: "RS256",
   },
 })
